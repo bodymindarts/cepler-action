96ce658b50efb35a3648456f437a5535233e4a62
Justin Carter <justin@misthos.io>
2020-10-16 11:44:36 +0200

    No need for double check

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,8 @@ cd ${1}
 export CEPLER_CONF=${2}
 export CEPLER_ENVIRONMENT=${3}
 
-cepler check
-
 case `cepler check; echo $?` in
   2)  echo "::set-output name=needs_deploying::false" ;;
   0)  echo "::set-output name=needs_deploying::true" ;;
-  *)  echo "Error running 'cepler check'" && exit 1
+  *)  echo "Error running 'cepler check'" && exit 1 ;;
 esac
