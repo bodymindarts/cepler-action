aa0c8e219c6ebee74b17f1284a56f7120d3e4d42
Justin Carter <justin@misthos.io>
2020-10-16 11:54:17 +0200

    Don't always error

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ cd ${1}
 
 case `cepler check; echo $?` in
   2)  echo "::set-output name=needs_deploying::false" ;;
+  1)  echo "Error running 'cepler check'" && exit 1 ;;
   0)  echo "::set-output name=needs_deploying::true" ;;
-  *)  echo "Error running 'cepler check'" && exit 1 ;;
 esac
