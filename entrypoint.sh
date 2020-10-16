4c60109367392b6f4896bf4b76a31c90ffec5b63
Justin Carter <justin@misthos.io>
2020-10-16 12:35:00 +0200

    Fix case

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 cd ${1}
 
-case `cepler check; echo $?` in
+cepler check
+
+case $? in
   2)  echo "::set-output name=needs_deploying::false" ;;
   1)  echo "Error running 'cepler check'" && exit 1 ;;
   0)  echo "::set-output name=needs_deploying::true" ;;
