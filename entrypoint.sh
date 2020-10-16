b94cb5249d984e6c148d7921d1d04f41a0500ebc
Justin Carter <justin@misthos.io>
2020-10-16 11:43:34 +0200

    Syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,4 @@ case `cepler check; echo $?` in
   2)  echo "::set-output name=needs_deploying::false" ;;
   0)  echo "::set-output name=needs_deploying::true" ;;
   *)  echo "Error running 'cepler check'" && exit 1
-fi
-
+esac
