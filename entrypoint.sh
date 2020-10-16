30d1849d43aaaccfa107b3ddcd4e368c56d8b982
Justin Carter <justin@misthos.io>
2020-10-16 12:46:56 +0200

    Conditionally run prepare or record

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,11 @@ case $? in
   1)  echo "Error running 'cepler check'" && exit 1 ;;
   0)  echo "::set-output name=needs_deploying::true" ;;
 esac
+
+if [ "${PREPARE}" -eq "true" ]; then
+  cepler prepare --force-clean
+fi
+
+if [ "${RECORD}" -eq "true" ]; then
+  cepler record --reset-head
+fi
